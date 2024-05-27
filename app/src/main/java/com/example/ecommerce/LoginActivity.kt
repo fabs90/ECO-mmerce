c5305d8e03f70140a2ca8de1b2fdbafe5c6f2d2f
unknown <fabianjuliansyah89@gmail.com>
2024-05-27 11:27:31 +0700

    add Login Activity without Google Button

--- app/src/main/java/com/example/ecommerce/LoginActivity.kt
+++ app/src/main/java/com/example/ecommerce/LoginActivity.kt
@@ -1,32 +1,20 @@
 package com.example.ecommerce
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.ecommerce.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btnMoveActivity: Button = findViewById(R.id.btn1)
-        btnMoveActivity.setOnClickListener {
-            val moveIntent = Intent(this@MainActivity, LoginActivity::class.java)
-            startActivity(moveIntent)
-        }
-
-
-
     }
 }
