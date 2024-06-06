955338cfb310298550c470ac37143e5daac6f8a2
unknown <fabianjuliansyah89@gmail.com>
2024-06-07 06:21:25 +0700

    Fix firebase assistant

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,17 +31,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         viewBinding = true
     }
 }
 
 dependencies {
-
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -51,9 +51,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    
-    implementation ("com.google.android.material:material:1.6.0")
-    implementation ("de.hdodenhof:circleimageview:3.1.0")
-    implementation ("com.etebarian:meow-bottom-navigation:1.3.1")
 
+    implementation(libs.material.v160)
+    implementation(libs.circleimageview)
+    // Hapus dependensi yang dikomentari jika tidak diperlukan
+    // implementation("com.etebarian:meow-bottom-navigation:1.3.1")
 }
--- app/src/main/java/com/example/ecommerce/view/MainActivity.kt
+++ app/src/main/java/com/example/ecommerce/view/MainActivity.kt
@@ -11,8 +11,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ecommerce.R
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
+class MainActivity : AppCompatActivity() {
+// Nanti tambahin ini setelah AppCOmpatActivity()
+    // , View.OnClickListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,24 +23,24 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btnMoveActivity: Button = findViewById(R.id.btn1)
-        val btnMoveActivity2: Button = findViewById(R.id.btn2)
-        btnMoveActivity.setOnClickListener(this)
-        btnMoveActivity2.setOnClickListener(this)
+//        val btnMoveActivity: Button = findViewById(R.id.btn1)
+//        val btnMoveActivity2: Button = findViewById(R.id.btn2)
+//        btnMoveActivity.setOnClickListener(this)
+//        btnMoveActivity2.setOnClickListener(this)
 
     }
 
-    override fun onClick(v : View?){
-        when (v?.id) {
-            R.id.btn1 -> {
-                val moveIntent = Intent(this@MainActivity, WelcomeActivity::class.java)
-                startActivity(moveIntent)
-            }
-            R.id.btn2 -> {
-                val moveIntent = Intent(this@MainActivity, LoginActivity::class.java)
-                startActivity(moveIntent)
-            }
-        }
-    }
+//    override fun onClick(v : View?){
+//        when (v?.id) {
+//            R.id.btn1 -> {
+//                val moveIntent = Intent(this@MainActivity, WelcomeActivity::class.java)
+//                startActivity(moveIntent)
+//            }
+//            R.id.btn2 -> {
+//                val moveIntent = Intent(this@MainActivity, LoginActivity::class.java)
+//                startActivity(moveIntent)
+//            }
+//        }
+//    }
 
 }
